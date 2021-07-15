b626f8c0c994d6d71a8e19154f0f3c8fb8158702
Nico Vibert <nvibert@vmware.com>
2021-07-15 13:43:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    nsxt = {
+      source = "vmware/nsxt"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
 provider "nsxt" {
   host                 = var.host
   vmc_token            = var.vmc_token
@@ -33,62 +42,27 @@ resource "nsxt_policy_segment" "segment12" {
     dhcp_ranges       = [var.Subnet12dhcp]
   }
 }
-resource "nsxt_policy_segment" "segment13" {
-  display_name        = "segment13"
-  description         = "Terraform provisioned Segment"
-  connectivity_path   = "/infra/tier-1s/cgw"
-  transport_zone_path = data.nsxt_policy_transport_zone.TZ.path
-  subnet {
-    cidr = var.Subnet13gw
-    dhcp_ranges = [var.Subnet13dhcp]
-  }
-}
-resource "nsxt_policy_segment" "segment14" {
-  display_name        = "segment14"
-  description         = "Terraform provisioned Segment"
-  connectivity_path   = "/infra/tier-1s/cgw"
-  transport_zone_path = data.nsxt_policy_transport_zone.TZ.path
-  subnet {
-    cidr = var.Subnet14gw
-  }
-}
 
-/*===================
-Create Network Groups
-====================*/
+/*========================================
+Create Security Group based on IP address
+=========================================*/
 
 
-resource "nsxt_policy_group" "group12" {
-  display_name = "tf-group12"
+resource "nsxt_policy_group" "ip-address-based-group" {
+  display_name = "ip-address based group"
   description  = "Terraform provisioned Group"
   domain       = "cgw"
   criteria {
     ipaddress_expression {
-      ip_addresses = [var.Subnet12]
-    }
-  }
-}
-resource "nsxt_policy_group" "group13" {
-  display_name = "tf-group13"
-  description  = "Terraform provisioned Group"
-  domain       = "cgw"
-  criteria {
-    ipaddress_expression {
-      ip_addresses = [var.Subnet13]
-    }
-  }
-}
-resource "nsxt_policy_group" "group14" {
-  display_name = "tf-group14"
-  description  = "Terraform provisioned Group"
-  domain       = "cgw"
-  criteria {
-    ipaddress_expression {
-      ip_addresses = [var.Subnet14]
+      ip_addresses = [var.SubnetCriteria]
     }
   }
 }
 
+/*=====================================
+Create Security Group based on VM Name
+======================================*/
+  
 resource "nsxt_policy_group" "name-based-group" {
   display_name = "name-based group"
   description  = "Terraform provisioned Group"
@@ -98,47 +72,13 @@ resource "nsxt_policy_group" "name-based-group" {
             key         = "Name"
             member_type = "VirtualMachine"
             operator    = "CONTAINS"
-            value       = "photo"
-        }
-      condition {
-            key         = "OSName"
-            member_type = "VirtualMachine"
-            operator    = "CONTAINS"
-            value       = "Ubuntu"
+            value       = [var.NameCriteria]
         }
     }
   
   
 }
 
-/*==============
-Create NAT group
-===============*/
-resource "nsxt_policy_group" "Photo_Private_IP" {
-  display_name = "Photo_Private_IP"
-  description  = "Terraform provisioned Group"
-  domain       = "cgw"
-  criteria {
-    ipaddress_expression {
-      ip_addresses = [cidrhost(var.Subnet13, 200)]
-    }
-  }
-}
-
-/*=====================================
-Create NAT rule
-======================================*/
-resource "nsxt_policy_nat_rule" "PhotoApp_NAT" {
-  display_name         = "PhotoApp_NAT"
-  action               = "DNAT"
-  source_networks      = []
-  destination_networks = [var.Photo_IP]
-  translated_networks  = [cidrhost(var.Subnet13, 200)]
-  gateway_path         = "/infra/tier-1s/cgw"
-
-  logging              = false
-  firewall_match       = "MATCH_INTERNAL_ADDRESS"
-}
 
 /*=====================================
 Create Security Group based on NSX Tags
@@ -157,24 +97,11 @@ resource "nsxt_policy_group" "Blue_VMs" {
   }
 }
 
-resource "nsxt_policy_group" "Red_VMs" {
-  display_name = "Red_VMs"
-  description  = "Terraform provisioned Group"
-  domain       = "cgw"
-  criteria {
-    condition {
-      key = "Tag"
-      member_type = "VirtualMachine"
-      operator = "EQUALS"
-      value = "Red|NSX_tag"
-    }
-  }
-}
 
 /*=====================================
 Create DFW rules
 ======================================*/
-resource "nsxt_policy_security_policy" "Terraform_section_1" {
+resource "nsxt_policy_security_policy" "Terraform_section" {
   display_name = "Terraform_section"
   description = "Terraform provisioned Security Policy"
   category = "Application"
@@ -209,7 +136,7 @@ resource "nsxt_policy_security_policy" "Colors" {
     source_groups = [
       nsxt_policy_group.Blue_VMs.path]
     destination_groups = [
-      nsxt_policy_group.Red_VMs.path]
+      nsxt_policy_group.ip-address-based-group.path]
     action = "DROP"
     services = ["/infra/services/ICMP-ALL"]
     logged = true
@@ -217,7 +144,7 @@ resource "nsxt_policy_security_policy" "Colors" {
   rule {
     display_name = "Red2Blue"
     source_groups = [
-      nsxt_policy_group.Red_VMs.path]
+      nsxt_policy_group.ip-address-based-group.path]
     destination_groups = [
       nsxt_policy_group.Blue_VMs.path]
     action = "DROP"
